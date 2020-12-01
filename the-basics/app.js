b6df6c955208b3f80f707e3fb0b43bc86be38af5
bbrown <brandon.brown39@gmail.com>
2020-11-30 18:45:18 -0800

    routing requests

--- the-basics/app.js
+++ the-basics/app.js
@@ -1,13 +1,15 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-  console.log(req.url, req.method, req.headers);
+  const url = req.url;
+  if (url === '/') {
+    res.write('<html>');
+    res.write('<head><title>Enter message</title></head>');
+    res.write('<body><form></form></body>');
+    res.write('</html>');
+  }
 
   res.setHeader('Content-Type', 'text/html');
-  res.write('<html>');
-  res.write('<head><title>My page</title></head>');
-  res.write('<body><h1>Hello from my NodeJS server</h1></body>');
-  res.write('</html>');
 
   res.end();
 });
