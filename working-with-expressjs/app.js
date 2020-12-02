1c2be4d5d8138b911550ee23a6a74131126adf3c
bbrown <brandon.brown39@gmail.com>
2020-12-01 18:07:47 -0800

    Add 404 page Filtering paths

--- working-with-expressjs/app.js
+++ working-with-expressjs/app.js
@@ -6,8 +6,12 @@ const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(adminRoutes);
+app.use('/admin', adminRoutes);
 app.use(shopRoutes);
+
+app.use((req, res, next) => {
+  res.status(404).send('<h1>404 - Page not found</h1>');
+});
 
 const PORT = process.env.PORT || 3000;
 
