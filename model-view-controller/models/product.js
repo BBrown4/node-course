c892ca3b1a2e7a1127d844c7f81b8c072568cfb9
BBrown <bmxchamp4@gmail.com>
2020-12-03 15:40:13 -0800

    Fetching data from files

--- model-view-controller/models/product.js
+++ model-view-controller/models/product.js
@@ -1,14 +1,45 @@
-const products = [];
+const path = require('path');
+const fs = require('fs');
+
 module.exports = class Product {
   constructor(title) {
     this.title = title;
   }
 
   save() {
-    products.push(this);
+    const p = path.join(
+      path.dirname(require.main.filename),
+      'data',
+      'products.json'
+    );
+
+    fs.readFile(p, (err, data) => {
+      let products = [];
+
+      if (!err) {
+        products = JSON.parse(data);
+      }
+
+      products.push(this);
+      fs.writeFile(p, JSON.stringify(products), err => {
+        console.log(err);
+      });
+    });
   }
 
-  static fetchAll() {
-    return products;
+  static fetchAll(cb) {
+    const p = path.join(
+      path.dirname(require.main.filename),
+      'data',
+      'products.json'
+    );
+
+    fs.readFile(p, (err, data) => {
+      if (err) {
+        cb([]);
+      }
+
+      cb(JSON.parse(data));
+    });
   }
 };
