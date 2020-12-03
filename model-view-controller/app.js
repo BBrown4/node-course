a20850b078e69dfa7d167d2288158e8577d83233
BBrown <bmxchamp4@gmail.com>
2020-12-03 15:00:35 -0800

    Implementing MVC model

---
+++ model-view-controller/app.js
@@ -0,0 +1,34 @@
+const express = require('express');
+// const expressHbs = require('express-handlebars');
+const bodyParser = require('body-parser');
+const path = require('path');
+const app = express();
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+const errorController = require('./controllers/error');
+
+// app.engine(
+//   'hbs',
+//   expressHbs({
+//     layoutsDir: 'views/layouts/',
+//     defaultLayout: 'main-layout',
+//     extname: 'hbs',
+//   })
+// );
+// app.set('view engine', 'hbs');
+
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
+
+app.use(errorController.get404);
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`);
+});
