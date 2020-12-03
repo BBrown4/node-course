8f99f9295ff2b5a4ccf6c9f93075c72a4e89327a
bbrown <brandon.brown39@gmail.com>
2020-12-02 18:02:00 -0800

    converting to EJS

--- dynamic-content-and-templating-engines/app.js
+++ dynamic-content-and-templating-engines/app.js
@@ -1,20 +1,20 @@
 const express = require('express');
-const expressHbs = require('express-handlebars');
+// const expressHbs = require('express-handlebars');
 const bodyParser = require('body-parser');
 const path = require('path');
 const app = express();
 const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
-app.engine(
-  'hbs',
-  expressHbs({
-    layoutsDir: 'views/layouts/',
-    defaultLayout: 'main-layout',
-    extname: 'hbs',
-  })
-);
-app.set('view engine', 'hbs');
+// app.engine(
+//   'hbs',
+//   expressHbs({
+//     layoutsDir: 'views/layouts/',
+//     defaultLayout: 'main-layout',
+//     extname: 'hbs',
+//   })
+// );
+app.set('view engine', 'ejs');
 app.set('views', 'views');
 
 app.use(bodyParser.urlencoded({ extended: false }));
