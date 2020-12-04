0b8be55b5476e0c58e4ff30358870ebf282a212a
BBrown <bmxchamp4@gmail.com>
2020-12-03 18:03:00 -0800

    Displaying product data

--- enhancing-the-app/models/product.model.js
+++ enhancing-the-app/models/product.model.js
@@ -17,9 +17,13 @@ const getProductsFromFile = cb => {
   });
 };
 
+// Product class
 module.exports = class Product {
-  constructor(title) {
+  constructor(title, imageUrl, description, price) {
     this.title = title;
+    this.imageUrl = imageUrl;
+    this.description = description;
+    this.price = price;
   }
 
   save() {
